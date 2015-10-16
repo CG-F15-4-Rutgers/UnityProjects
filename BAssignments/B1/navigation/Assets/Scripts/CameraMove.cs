ef04469d1db67c15a49f0a259c8bac0bb6e18603
Anisha Mohan <anisha0713@gmail.com>
2015-10-16 02:12:04 -0400

    Added navigation maps, click to create target, and started animation

---
+++ BAssignments/B1/navigation/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+	}
+}
---
+++ BAssignments/B1/navigation/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform target;
+    NavMeshAgent agent;
+    public Camera camera;
+
+    void Start () {
+    
+        agent = GetComponent<NavMeshAgent> ();
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
