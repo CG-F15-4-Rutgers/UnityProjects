5ff9deec422f24a9b2543eaf60c8567d9f93c840
Anisha Mohan <anisha0713@gmail.com>
2015-10-18 03:39:48 -0400

    Started animation, has moving forward, jumping, and sprinting
    To add: need to move character according to arrow keys

---
+++ BAssignments/B1/animation/animation/Assets/KyleAnimationScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KyleAnimationScript : MonoBehaviour {
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Vertical");
+		anim.SetFloat("Speed", move);
+
+		if(Input.GetKeyDown(KeyCode.RightShift)) {
+			anim.SetBool("Sprint", true);
+		}
+
+		if(Input.GetKeyUp(KeyCode.RightShift)) {
+			anim.SetBool("Sprint", false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			anim.SetTrigger("Jump");
+		}
+	}
+}
---
+++ BAssignments/B1/animation/animation/Assets/PlayerAnimationScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationScript : MonoBehaviour {
+	public Animator anim;
+	int sprintHash = Animator.StringToHash("JogForward_NtrlFaceFwd");
+	int walkHash = Animator.StringToHash("WalkFWD");
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Vertical");
+		anim.SetFloat("Speed", move);
+
+		/*if(Input.GetKeyDown(KeyCode.RightShift)) {
+			anim.SetBool("Sprint", true);
+		}
+
+		if(Input.GetKeyUp(KeyCode.RightShift)) {
+			anim.SetBool("Sprint", false);
+		}
+*/
+		/*AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			anim.SetTrigger("Walk Jump");
+		}
+
+		/*if(Input.GetKeyUp(KeyCode.Space)) {
+			anim.SetBool("Walk Jump", false);
+		}	*/
+	}	
+}
