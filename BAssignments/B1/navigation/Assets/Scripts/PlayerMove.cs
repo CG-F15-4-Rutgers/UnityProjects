0ec55741eabfed96d281a2ca8cbcd6b9a4392f88
Chang <chang@nbp-245-114.nbp.ruw.rutgers.edu>
2015-10-18 11:55:40 -0400

    Added kyle to map with navigation

--- BAssignments/B1/navigation/Assets/Scripts/PlayerMove.cs
+++ BAssignments/B1/navigation/Assets/Scripts/PlayerMove.cs
@@ -8,20 +8,27 @@ public class PlayerMove : MonoBehaviour {
     public Transform target;
     NavMeshAgent agent;
     public Camera camera;
-
+	public Animator anim;
+	public float speed;
+	
+	// Use this for initialization
     void Start () {
     
         agent = GetComponent<NavMeshAgent> ();
+		anim = GetComponent<Animator>();
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         
         if (Physics.Raycast(ray, out hit)) {
             Transform objectHit = hit.transform;
         }
+
     }
     
     // Update is called once per frame
     void Update () {
+		speed = agent.acceleration;
+		anim.SetFloat("Speed", speed);
         if (Input.GetMouseButtonDown(0)) {
             RaycastHit hit;
             Ray ray = camera.ScreenPointToRay (Input.mousePosition);
